461cc78d21c4cb5ea739844363da25e89b778538
tpham <taiphamhuu56ld3@gmail.com>
2024-07-03 20:46:22 +0700

    [FEAT] NOTASK: Remove toggle icon and navbar

--- js/script.js
+++ js/script.js
@@ -31,4 +31,8 @@ window.onscroll = () => {
     let header = document.querySelector('header');
 
     header.classList.toggle('sticky', window.scrollY > 100);
+
+    // remove toggle icon and navbar
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
 }
