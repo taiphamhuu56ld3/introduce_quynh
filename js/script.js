0813f491d0f9726976f332b75bb0ee98c7225054
tpham <taiphamhuu56ld3@gmail.com>
2024-07-20 20:49:32 +0700

    [FEAT] Q2: Sending mail

--- js/script.js
+++ js/script.js
@@ -47,3 +47,35 @@ window.onscroll = () => {
 
     footer.classList.toggle('show-animate', this.innerHeight + this.scrollY + 50 >= document.scrollingElement.scrollHeight);
 }
+
+// send email
+let form = document.querySelector('form');
+const fullName = document.getElementById("name");
+const email = document.getElementById("email");
+const phone = document.getElementById("phone");
+const subject = document.getElementById("subject");
+const message = document.getElementById("message");
+
+function sendEmail() {
+
+    const bodyMessage = `Full Name: ${fullName}<br> Email: ${email.value}
+    <br> Phone Number: ${phone.value}<br> Message: ${message.value}`;
+
+    Email.send({
+        Host : "smtp.elasticemail.com",
+        Username : "taiphamhuu56ld3@gmail.com",
+        Password : "88E402CC3F90E1AED07B942C572CFE52C53A",
+        To : 'taiphamhuu56ld3@gmail.com',
+        From : "taiphamhuu56ld3@gmail.com",
+        Subject : subject.value,
+        Body : bodyMessage
+    }).then(
+      message => alert(message)
+    );
+}
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    sendEmail();
+});
