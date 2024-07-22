64745e21255a8296d143b9666eae93c972045b59
tpham <taiphamhuu56ld3@gmail.com>
2024-07-22 23:37:29 +0700

    [FEAT] Q3: Updated controls to be able to send emails

--- js/script.js
+++ js/script.js
@@ -82,8 +82,69 @@ function sendEmail() {
     );
 }
 
+function checkInputs() {
+    const items = document.querySelectorAll(".item");
+
+    for (const item of items) {
+        if (item.value == "") {
+            item.classList.add("error");
+            item.parentElement.classList.add("error");
+        }
+
+        if (items[1].value != "") {
+            checkEmail();
+        }
+
+        items[1].addEventListener("keyup", () => {
+            checkEmail();
+        });
+
+        item.addEventListener("keyup", () => {
+            if (item.value != "") {
+                item.classList.remove("error");
+                item.parentElement.classList.remove("error");
+            }
+            else {
+                item.classList.add("error");
+                item.parentElement.classList.add("error");
+            }
+        });
+    }
+}
+
+function checkEmail() {
+    const emailRegex = /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,3})(\.[a-z]{2,3})?$/;
+    const errorTxtEmail = document.querySelector(".error-txt.email");
+
+    if (!email.value.match(emailRegex)) {
+        email.classList.add("error");
+        email.parentElement.classList.add("error");
+
+        if (email.value != "") { 
+            errorTxtEmail.innerText  = "Enter a valid email address";
+        }
+        else {
+            errorTxtEmail.innerText = "Email Address can't be blank";
+        }
+    }
+    else {
+        email.classList.remove("error");
+        email.parentElement.classList.remove("error");
+    }
+}
+
 form.addEventListener("submit", (e) => {
     e.preventDefault();
+    checkInputs();
 
-    sendEmail();
+    if (!fullName.classList.contains("error") && 
+    !email.classList.contains("error") && 
+    !phone.classList.contains("error") && 
+    !subject.classList.contains("error") && 
+    !message.classList.contains("error")) {
+        sendEmail();
+
+        form.reset();
+        return false;
+    }
 });
