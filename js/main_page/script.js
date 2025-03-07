1e046d87d3bd2ecde7b8a1d1d4df29a5932466a1
tpham <taiphamhuu56ld3@gmail.com>
2025-03-07 22:59:48 +0700

    [FEAT] IT1: Update cursor

--- js/main_page/script.js
+++ js/main_page/script.js
@@ -178,3 +178,17 @@ const handleDropdownClicked = (type) => {
             console.warn('Unknown dropdown type');
     }
 };
+
+const cursor = document.querySelector('.cursor');
+
+document.addEventListener('mousemove', e => {
+    cursor.setAttribute("style", "top: "+(e.pageY - 10)+"px; left: "+(e.pageX - 10)+"px;")
+})
+
+document.addEventListener('click', () => {
+    cursor.classList.add("expand");
+
+    setTimeout(() => {
+        cursor.classList.remove("expand");
+    }, 500)
+})
