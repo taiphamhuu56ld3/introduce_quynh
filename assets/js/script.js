87d8ad9337833288fc61d93640702cca602f8462
tpham <taiphamhuu56ld3@gmail.com>
2025-03-22 17:54:23 +0700

    [FEAT] #1 IT2: Enable horizontal navbar in viewports smaller than 1200px

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,23 @@
+// Enable horizontal navbar in viewports smaller than 1200px
+
+const addEventOnElements = function (elements, eventType, callback) {
+  for (let i = 0, len = elements.length; i < len; i++) {
+    elements[i].addEventListener(eventType, callback);
+  }
+};
+
+const navbar = document.querySelector("[data-navbar]");
+const navTogglers = document.querySelectorAll("[data-nav-toggler]");
+
+const toggleNav = () => {
+  navbar.classList.toggle("active");
+  document.body.classList.toggle("nav-active");
+}
+
+addEventOnElements(navTogglers, "click", toggleNav);
+
 // Scroll to top effect and header effect
+
 const header = document.querySelector("[data-header]");
 const backToBtn = document.querySelector("[data-back-top-btn]");
 
