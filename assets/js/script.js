9273e520b992b3a0c74260128643e57d64a7818f
tpham <taiphamhuu56ld3@gmail.com>
2025-03-22 17:36:39 +0700

    [FEAT] #1 IT2: Add feature left and right slider in section topic

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,3 +8,54 @@ window.addEventListener("scroll", () => {
     backToBtn.classList.remove("active");
   }
 });
+
+// Left and right slider in section topic
+
+const slider = document.querySelector("[data-slider]");
+const sliderContainer = document.querySelector("[data-slider-container]");
+
+const sliderPrevBtn = document.querySelector("[data-slider-prev]");
+const sliderNextBtn = document.querySelector("[data-slider-next]");
+
+let totalSliderVisibleItems = Number(
+  getComputedStyle(slider).getPropertyValue("--slider-items")
+);
+
+let totalSlidableItems =
+  sliderContainer.childElementCount - totalSliderVisibleItems;
+
+let currentSlidePos = 0;
+
+const moveSliderItem = function () {
+  sliderContainer.style.transform = `translateX(-${sliderContainer.children[currentSlidePos].offsetLeft}px)`;
+};
+
+// Next slide
+
+const slideNext = function () {
+  const slideEnd = currentSlidePos > totalSlidableItems;
+
+  if (slideEnd) {
+    currentSlidePos = 0;
+  } else {
+    currentSlidePos++;
+  }
+
+  moveSliderItem();
+};
+
+sliderNextBtn.addEventListener("click", slideNext);
+
+// Previous slide
+
+const slidePrev = function () {
+  if (currentSlidePos < 0) {
+    currentSlidePos = totalSlidableItems;
+  } else {
+    currentSlidePos--;
+  }
+
+  moveSliderItem();
+};
+
+sliderPrevBtn.addEventListener("click", slidePrev);
